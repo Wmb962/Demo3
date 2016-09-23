21eb49a38456c815665252c4c3ece7cb1d5e157f
wu <38723540@qq.com>
2016-09-23 20:28:24 +0800

    侧滑菜单高仿手机qq和今日看点
    仅供学习，框架后续完成。AndroidStudio直接导入即可运行

---
+++ app/src/main/java/wumingbing/com/demo/fragment/MyCommentsFragment.java
@@ -0,0 +1,47 @@
+package wumingbing.com.demo.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import wumingbing.com.demo.R;
+
+/**
+ * 
+ * @author wumingbing
+ * @description 今日
+ */
+public class MyCommentsFragment extends Fragment {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+	
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.frag_mycomments, null);
+		return view;
+	}
+	
+	
+	
+	@Override
+	public void onDestroyView() {
+		super.onDestroyView();
+	}
+	
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+	}
+}
